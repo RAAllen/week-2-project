2e2f784572cc8f413a7d37d5d804b666d6245479
Rebecca Allen <rebeccazarsky@gmail.com>
2016-08-12 11:43:40 -0400

    start the scripts.js work, add in additional tags for this in index.html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+  var "cAnswer" = 1
+  var "javaAnswer" = 2
+  var "rubyAnswer" = 3
+  var "phpAnswer" = 4
+  var "cssAnswer" = 5
+  var questionOneValue =
+
+
+  });
+});
